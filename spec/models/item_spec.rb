af67d4841bd4d50b66fe81ea24c7942d121b15b6
mathieumllt <mathieu.maillet@ymail.com>
2019-01-08 14:02:17 +0100

    factories + rspec tests

--- spec/models/item_spec.rb
+++ spec/models/item_spec.rb
@@ -21,7 +21,10 @@ RSpec.describe Item, type: :model do
     describe 'Database' do
       it { is_expected.to have_db_column(:id).of_type(:integer) }
       it { is_expected.to have_db_column(:original_price).of_type(:float).with_options(null: false) }
+      it { is_expected.to have_db_column(:has_discount).of_type(:boolean).with_options(default: false) }
+      it { is_expected.to have_db_column(:discount_percentage).of_type(:integer).with_options(default: 0) }
       it { is_expected.to have_db_column(:created_at).of_type(:datetime).with_options(null: false) }
+      it { is_expected.to have_db_column(:updated_at).of_type(:datetime).with_options(null: false) }
     end
   end
 
@@ -32,4 +35,21 @@ RSpec.describe Item, type: :model do
       it { expect(item.price).to eq(80.00) }
     end
   end
+
+  describe 'Price without discount' do
+    context 'when the item has not a discount' do
+      let(:item) { build(:item_without_discount, original_price: 100.00, discount_percentage: nil) }
+
+      it { expect(item.price).to eq(100.00) }
+    end
+  end
+
+  describe 'Price cannot be 0' do
+    context 'when the discount_percentage is 100' do
+      let(:item) { build(:item_with_discount, original_price: 100.00, discount_percentage: 100) }
+
+      it { expect(item.price).to eq(0) }
+    end
+  end
+
 end
