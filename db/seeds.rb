ad8900be510084de9d8b4a59d61fe2acf04a5575
mathieumllt <mathieu.maillet@ymail.com>
2019-01-09 16:37:14 +0100

    Add and seed categories to the view

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,9 +1,19 @@
 # frozen_string_literal: true
 
+1.upto(5) do |i|
+  Category.create!(
+    name: Faker::Coffee.variety,
+    description: Faker::Lorem.sentence
+  )
+  p "CATEGORY #{i} : créé"
+end
+
 1.upto(10) do |i|
   Item.create!(
     original_price: Faker::Number.decimal(2),
-    name: Faker::Commerce.material
+    name: Faker::Coffee.blend_name,
+    category_id: rand(5)+1
   )
   p "ITEM #{i} : créé"
 end
+
