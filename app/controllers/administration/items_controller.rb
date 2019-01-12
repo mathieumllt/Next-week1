3412e9095921c32b1e36ca35f1887e929d8acaa8
mathieumllt <mathieu.maillet@ymail.com>
2019-01-12 12:17:07 +0100

    fix update item controller

--- app/controllers/administration/items_controller.rb
+++ app/controllers/administration/items_controller.rb
@@ -10,16 +10,10 @@ module Administration
       @item = Item.find(params[:id])
     end
 
-    # def update
-    #   @item = Item.find(params[:id])
-    #   @item.update(item_params)
-    #   redirect_to administration_items_path, alert: "L'item a bien été modifié."
-    # end
-
     def update
       @item = Item.find(params[:id])
       puts @item.id
-      if params[:item][:discount_percentage].to_f > 100
+      if params[:item][:discount_percentage].to_f >= 100
         flash[:alert] = "Veuillez entrer une réduction valable"
       else
         item_params = params.require(:item).permit(:discount_percentage)
