4b51df168b077360b2fef2e312ac4d6358bc03c7
mathieumllt <mathieu.maillet@ymail.com>
2019-01-15 21:01:49 +0100

    Install Devise + Create Models

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,16 @@
 # frozen_string_literal: true
 
 Rails.application.routes.draw do
-  root 'administration/items#index'
+  devise_for :users, controllers: { sessions: 'users/sessions', 
+ 			    					registrations: 'users/registrations',
+ 			    				    confirmations: 'users/confirmations'
+ 			    				  }
+  devise_for :admins, controllers: { sessions: 'admins/sessions', 
+  	                                 registrations: 'admins/registrations', 
+  	                                 confirmations: 'admins/confirmations'
+  	                               }
+
+  root to: "home#landing_page"
 
   get '/home', to: 'home#landing_page'
 
--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,25 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_01_09_105320) do
+ActiveRecord::Schema.define(version: 2019_01_15_150318) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admins", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_admins_on_reset_password_token", unique: true
+  end
 
   create_table "categories", force: :cascade do |t|
     t.string "name", null: false
@@ -31,6 +46,21 @@ ActiveRecord::Schema.define(version: 2019_01_09_105320) do
     t.string "name"
     t.bigint "category_id"
     t.index ["category_id"], name: "index_items_on_category_id"
+  end
+
+  create_table "users", force: :cascade do |t|
+    t.string "email", default: "", null: false
+    t.string "encrypted_password", default: "", null: false
+    t.string "reset_password_token"
+    t.datetime "reset_password_sent_at"
+    t.datetime "remember_created_at"
+    t.string "confirmation_token"
+    t.datetime "confirmed_at"
+    t.datetime "confirmation_sent_at"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_users_on_email", unique: true
+    t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
 
   add_foreign_key "items", "categories"
